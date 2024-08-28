a010b257884b5d3268c619e409f7e5bd5ae5ea82
evelyn <cyc.evelynchang@gmail.com>
2024-08-28 16:19:30 +0800

    feat: add order, oder detail dto.

--- appbot/src/main/java/com/example/appbot/dto/OrderDTO.java
+++ appbot/src/main/java/com/example/appbot/dto/OrderDTO.java
@@ -1,2 +1,21 @@
-package com.example.appbot.dto;public class OrderDTO {
+package com.example.appbot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDTO {
+    private Integer id;
+    private Integer lineUserId;
+    private Integer orderStatus;
+    private Integer total;
+    private LocalDateTime createAt;
+    private LocalDateTime lastModifiedAt;
 }
--- appbot/src/main/java/com/example/appbot/dto/OrderDetailDTO.java
+++ appbot/src/main/java/com/example/appbot/dto/OrderDetailDTO.java
@@ -1,2 +1,18 @@
-package com.example.appbot.dto;public class OrderDetailDTO {
+package com.example.appbot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDetailDTO {
+    private Integer id;
+    private Integer quantity;
+    private Integer orderId;
+    private Integer productId;
+    private Integer campaignId;
 }
