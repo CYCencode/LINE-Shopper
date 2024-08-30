4b5c8dd0aa9ad744de8601e253c7979379b0932e
Brian Chen <jeb.3332@gmail.com>
2024-08-30 10:56:04 +0800

    feat: create web page fix the security problem

--- appbot/src/main/java/com/example/appbot/controller/HomeController.java
+++ appbot/src/main/java/com/example/appbot/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.appbot.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public ResponseEntity<?> home() {
-        return ResponseEntity.ok("hello world");
+    public ResponseEntity<?> home(HttpServletRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/admin/order.html");
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
